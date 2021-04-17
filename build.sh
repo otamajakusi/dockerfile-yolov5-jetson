52b0f2d3edcf1bcbaa950c9e77db3c98a5489fe0
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2021-04-17 21:10:01 +0900

    support jetpack 4.5

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker build -t yolov5 .
+if [[ $(cat /etc/nv_tegra_release) =~ ^.*REVISION:[^\S]([0-9]*\.[0-9]).*$ ]]; then
+    case ${BASH_REMATCH[1]} in
+        5.*) docker build -t yolov5 . ;;
+        4.*) docker build -t yolov5 -f Dockerfile.jetpack4.4 . ;;
+        * ) echo "unknown jetpack ${BASH_REMATCH[1]}" ;;
+    esac
+fi
