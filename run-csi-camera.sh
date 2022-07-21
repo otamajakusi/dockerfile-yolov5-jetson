92dba039d7399645119ea0035807e53f26e24231
hiroyuki.obinata <hiroyuki.obinata@gmail.com>
2022-07-21 21:07:09 +0900

    update run-csi-camera.sh

--- run-csi-camera.sh
+++ run-csi-camera.sh
@@ -2,4 +2,12 @@
 xhost +local:
 # https://github.com/JetsonHacksNano/CSI-Camera
 wget https://raw.githubusercontent.com/JetsonHacksNano/CSI-Camera/master/simple_camera.py -O simple_camera.py
-docker run -it --rm --runtime nvidia --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v /tmp/argus_socket:/tmp/argus_socket -v $(pwd):/csi-camera yolov5 python3 /csi-camera/simple_camera.py
+docker run -it --rm \
+      --runtime nvidia \
+      --network host \
+      -e DISPLAY=$DISPLAY \
+      -e LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1 \
+      -v /tmp/.X11-unix/:/tmp/.X11-unix \
+      -v /tmp/argus_socket:/tmp/argus_socket \
+      -v $(pwd):/csi-camera \
+      yolov5 python3 /csi-camera/simple_camera.py
